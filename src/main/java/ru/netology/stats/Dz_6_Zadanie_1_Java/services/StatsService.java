7de3f40b7415dc990d222eb3e49ccf7ea0d07c26
VozunMaxim <vozun.maxim@gmail.com>
2022-07-26 11:28:41 +0300

    first commit

---
+++ src/main/java/ru/netology/stats/Dz_6_Zadanie_1_Java/services/StatsService.java
@@ -0,0 +1,80 @@
+package ru.netology.stats.Dz_6_Zadanie_1_Java.services;
+
+import java.util.Arrays;
+
+public class StatsService {
+    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    // 1 Метод ниже считает сумму всех продаж
+    public int totalSales(long[] sales) {
+        int total = 0;
+        for (int i = 0; i < sales.length; i++) {
+            total = (int) (total + sales[i]);
+        }
+        return total;
+    }
+
+    // 2 Метод ниже считает среднюю сумму продаж в месяц
+    public int averageSales(long[] sales) {
+        int total = 0;
+        for (int i = 0; i < sales.length; i++) {
+            total = (int) (total + sales[i]);
+        }
+        return (int) ((double) total / sales.length);
+    }
+
+    // 3 Метод ниже считает номер месяца максимальных продаж
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    // 4 Метод ниже считает номер месяца минимальных продаж
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    // 5 Метод ниже считает кол-во месяцев, в которых продажи были ниже среднего
+    public int lowAverageSales(long[] sales) {
+        int averageMonth = averageSales(sales);
+        int month = 0;
+        int count = 0;
+        for (long sale : sales) {
+            if (sale > averageMonth) {
+                continue;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    // 6 Метод ниже считает кол-во месяцев, в которых продажи были выше среднего
+    public int highAverageSales(long[] sales) {
+        int averageMonth = averageSales(sales);
+        int month = 0;
+        int count = 0;
+        for (long sale : sales) {
+            if (sale < averageMonth) {
+                continue;
+            }
+            count++;
+        }
+        return count;
+    }
+
+}
---
+++ src/test/java/ru/netology/stats/Dz_6_Zadanie_1_Java/services/StatsServiceTest.java
@@ -0,0 +1,69 @@
+package ru.netology.stats.Dz_6_Zadanie_1_Java.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @Test //1 Сумма всех продаж
+    public void shouldFindTotalsales() {
+        StatsService service = new StatsService();
+
+        int expected = 180;
+        int actual = service.totalSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // 2 Сумма средних продаж
+    public void shouldFindAverageSales() {
+        StatsService service = new StatsService();
+
+        int expected = -999;
+        int actual = service.averageSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //3 Месяц с максим. продажами
+    public void shouldFindMaxSales() {
+        StatsService service = new StatsService();
+
+        int expected = 8;
+        int actual = service.maxSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //4 Месяц с мин. продажами
+    public void shouldFindMinSales() {
+        StatsService service = new StatsService();
+
+        int expected = 9;
+        int actual = service.minSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //5 Кол-во месяцев с показателем ниже среднего
+    public void shouldFindLowAverageSales() {
+        StatsService service = new StatsService();
+
+        int expected = 7;
+        int actual = service.lowAverageSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //6 Кол-во месяцев с показателем выше среднего
+    public void shouldFindHighAverageSales() {
+        StatsService service = new StatsService();
+
+        int expected = 7;
+        int actual = service.highAverageSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
